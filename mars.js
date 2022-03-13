f0cff658b16109b38ce2470253533b815e9d2fbe
BorimirGanchev <borimir.k.ganchev.2021@elsys-bg.org>
2022-03-13 04:22:55 +0200

    added more back arrows on the sun and mars

--- mars.js
+++ mars.js
@@ -31,3 +31,21 @@ document.getElementById ("picturebutton1").onclick = function(){
         document.getElementById("firstPicture").src = "mars_3.jpg";
      }
 }
+
+document.getElementById("picturebutton2").onclick = function(){
+    swich--;
+    console.log(swich);
+    if(swich == 0){
+        swich = 3;
+    }
+    if(swich%3 == 1){
+        document.getElementById("firstPicture").src = "mars.jpg";
+    }
+     if(swich%3 == 2){
+        console.log(swich);
+        document.getElementById("firstPicture").src = "mars_2.jpg";
+    }
+     if(swich%3 == 0){
+        document.getElementById("firstPicture").src = "mars_3.jpg";
+     }
+    }
